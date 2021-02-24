75aa97410e52ffc69b1d88a0518810c67e872d76
Kaukakakak <76785280+Kaukakakak@users.noreply.github.com>
2021-02-24 12:11:44 -0300

    Add files via upload

---
+++ Komputer/suporte.java
@@ -0,0 +1,5 @@
+Pc = komputer * (sanbot3.0 + sanbot - s4anvic)
+           + 4 * bot; // PREFER
+
+Android = /0/android/suporte.java * (sanbot3.0 + s4anvic
+                       - sanbot) + 4 * bot; // AVOID
