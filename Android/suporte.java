25d6bc9a54e7f64335b5ece3608b6e0ad52f4c12
Kaukakakak <76785280+Kaukakakak@users.noreply.github.com>
2021-02-24 12:09:34 -0300

    Add files via upload

---
+++ Android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (sanbot3.0)
+ *
+ * Version information   (3.0)
+ *
+ * Date                  (06/12,15:35)
+ *
+ * author                (S4ANVIC TY)
+ * Copyright notice      (bot com suporte a Android/Java)
